ce735257395c4691660c11a288480657961e3605
Hamna-Sherin <hamnaadiyottil@gmail.com>
2025-07-04 22:36:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-var start= new Date().getTime()
-document.getElementById("box").onclick=function(){
+// variable declaration starting timer
+var start= new Date().getTime();
 
-    // computing time taken for a click
-    var end= new Date().getTime()
-    var timeTaken= (end-start)/1000
-    alert("clicked in " + timeTaken+ " s")
-
-    // generating a random color
+// function for random color generation
+function randomColor(){
     var digits = "0123456789ABCDEF"
     var color = "#"
     for(var i=0; i<6; i++){
         color += digits[Math.floor(Math.random()*16)]
     }
     document.getElementById("box").style.backgroundColor= color
+}
 
-    // resetting timer after a click
-    start= new Date().getTime()
+// function for random position
+function move(){
+    var left;
+    var right;
+    var wh;
+    left= Math.random()*300;
+    right= Math.random()*300;
+    wh= ((Math.random()*400)+100);
+    document.getElementById("box").style.left= left +"px";
+    document.getElementById("box").style.right= right+"px";
+    document.getElementById("box").style.width= wh+"px";
+    document.getElementById("box").style.height= wh+"px";
+    document.getElementById("box").style.display= "block";
+    start= new Date().getTime();
+}
+
+// defining onclick funtion
+document.getElementById("box").onclick= function(){
+    document.getElementById("box").style.display= "none";
+    var end= new Date().getTime();
+    var timeTaken= (end-start)/1000;
+    alert(timeTaken);
+    move();
+    randomColor();
 }
