ac8605524ffda53453a211fe6f468ab3d6a000c1
Hamna-Sherin <hamnaadiyottil@gmail.com>
2025-07-05 17:36:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.getElementById("box").onclick= function(){
     document.getElementById("box").style.display= "none";
     var end= new Date().getTime();
     var timeTaken= (end-start)/1000;
-    alert(timeTaken);
+    alert(timeTaken+"s");
     move();
     randomColor();
 }
